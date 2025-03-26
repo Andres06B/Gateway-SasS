b484bb9ef5da7f52b88e6cabbd83fde2bd88fc91
axel <davilachavesalejandro@gmail.com>
2025-03-26 12:00:05 -0500

    cree el popup e integre la landing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './view/landing/landing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LandingComponent }, // Página principal
+  { path: 'landing', component: LandingComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' } // Redirige rutas desconocidas a Landing
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
