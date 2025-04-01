1592bf9b979d840c721aabe621fd01114fbac939
axel <davilachavesalejandro@gmail.com>
2025-04-01 15:43:49 -0500

    actualice responsividad y añadi belleza

--- src/app/pages/Landing/landing.component.ts
+++ src/app/pages/Landing/landing.component.ts
@@ -2,34 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-landing',
+  standalone: false,
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.css']
 })
 export class LandingComponent {
-  // Control del popup
-  showPopup: boolean = false;
-
-  // Función para abrir el popup
-  openPopup(): void {
-    console.log('Abriendo popup'); // Para debug
-    this.showPopup = true;
-  }
-
-  // Función para cerrar el popup
-  closePopup(): void {
-    this.showPopup = false;
-  }
-
-  // Función para manejar la selección
-  selectUserType(userType: 'admin' | 'user'): void {
-    console.log(`Tipo seleccionado: ${userType}`);
-    this.closePopup();
-    
-    // Aquí puedes añadir la lógica de redirección
-    if (userType === 'admin') {
-      // Redirigir a administrador
-    } else {
-      // Redirigir a usuario
-    }
-  }
+  
 }
