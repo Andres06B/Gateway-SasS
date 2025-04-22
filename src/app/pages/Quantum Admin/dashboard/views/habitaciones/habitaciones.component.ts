1d0fed327e273329156cd40dfe0dfc3fa6ec00b3
Milton Jose <berriomilton3@gmail.com>
2025-04-22 17:40:11 -0500

    servicio de habitaciones listos

--- src/app/pages/Quantum Admin/dashboard/views/habitaciones/habitaciones.component.ts
+++ src/app/pages/Quantum Admin/dashboard/views/habitaciones/habitaciones.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { rooms } from '../interfaces/rooms.interface';
+import { RoomsService } from '../../../../../service/rooms.service';
 @Component({
   selector: 'app-habitaciones',
   standalone: false,
@@ -7,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrl: './habitaciones.component.css'
 })
 export class HabitacionesComponent {
+  Rooms: rooms[] = []
+  constructor(private habitacionesservices:RoomsService){
 
+  }
+    ngOInit(){
+      this.Habitaciones()
+    }
+    Habitaciones(){
+      this.habitacionesservices.getRooms().subscribe(
+        (data)=>{
+          this.Rooms=data
+          console.log(this.Rooms)
+        }
+      )
+    }
 }
--- src/app/service/rooms.service.ts
+++ src/app/service/rooms.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { rooms } from '../pages/Quantum Admin/dashboard/views/interfaces/rooms.interface';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class RoomsService {
-
-  constructor() { }
+  private apiURL = 'http://localhost:3000/rooms';
+  
+  constructor(
+    private http: HttpClient
+  ) { }
+  
+  getRooms(): Observable<rooms[]>{
+    return this.http.get<rooms[]>(this.apiURL + '/all');
+  }
 }
