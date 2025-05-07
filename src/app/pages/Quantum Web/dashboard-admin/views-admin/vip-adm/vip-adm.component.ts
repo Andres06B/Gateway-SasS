8e67e8976feb24a35b2e33502725bcb1f6562413
Eduardo <solanoe934@gmail.com>
2025-05-07 06:58:10 -0500

    feature: funcionamiento del dashboard administrativo nuestro

--- src/app/pages/Quantum Web/dashboard-admin/views-admin/vip-adm/vip-adm.component.ts
+++ src/app/pages/Quantum Web/dashboard-admin/views-admin/vip-adm/vip-adm.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { paymentService } from '../../../../../interface/payment.interface';
+import { PaymentServiceService } from '../../../../../service/payment-service.service';
 
 @Component({
   selector: 'app-vip-adm',
@@ -7,5 +9,33 @@ import { Component } from '@angular/core';
   styleUrl: './vip-adm.component.css'
 })
 export class VipAdmComponent {
-
+  payments: paymentService[] = [];
+    totalBalance: number = 0;
+  
+    constructor(
+      private service: PaymentServiceService
+    ){}
+  
+    ngOnInit(): void {
+      this.findAll();
+    }
+  
+    findAll(): void {
+      this.service.findVipUsers().subscribe({
+        next: (res) => {
+          this.payments = res;
+          console.log(this.payments);
+          this.calculateVipIncome();
+        }
+        , error: (err) => {
+          console.log(err);
+        }
+      });
+    }
+  
+    calculateVipIncome(): void {
+      this.totalBalance = this.payments.reduce((total, payment) =>{
+         return total + payment.price
+      } , 0);
+    }
 }
--- src/app/service/payment-service.service.ts
+++ src/app/service/payment-service.service.ts
@@ -1,9 +1,35 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaymentServiceService {
 
-  constructor() { }
+  private baseUrl = 'http://localhost:3000/payment'
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  findAll(): Observable<any[]> {
+    return this.http.get<any[]>(this.baseUrl + '/all');
+  }
+
+  findPremiumUsers(): Observable<any[]> {
+    return this.http.get<any[]>(this.baseUrl + '/users/premium');
+  }
+
+  findVipUsers(): Observable<any[]> {
+    return this.http.get<any[]>(this.baseUrl + '/users/vip');
+  }
+
+  
+
+
+
+
+
+
 }
