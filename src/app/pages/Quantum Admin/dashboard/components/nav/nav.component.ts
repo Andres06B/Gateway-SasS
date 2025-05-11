28bcd67ea5d498468db3b33700d0bb48840cc584
Eduardo <solanoe934@gmail.com>
2025-05-11 16:46:47 -0500

    mejoras en la navegación y gestión de usuarios, incluyendo la visualización de información del administrador y la mejora de la gestión de errores en el inicio de sesión

--- src/app/pages/Quantum Admin/dashboard/components/nav/nav.component.ts
+++ src/app/pages/Quantum Admin/dashboard/components/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, HostListener } from '@angular/core';
+import { UsersService } from '../../../../../service/users.service';
 
 @Component({
   selector: 'app-nav',
@@ -7,11 +8,41 @@ import { Component, HostListener } from '@angular/core';
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
-
+  admin: number = Number(localStorage.getItem('token'));
   isMenuOpen = false;
+  adminInfo: { name: string; last_name: string; email: string } | null = null;
 
   toggleMenu(): void {
     this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  ngOnInit(): void {
+    this.adminData();
+  }
+
+  constructor(
+    private adminS: UsersService
+  ) {}
+
+  adminData():void {
+    this.adminS.getUser(this.admin).subscribe({
+      next: res => {
+        this.adminInfo = {
+          name: res.name,
+          last_name: res.last_name,
+          email: res.email
+        };
+      }
+    });
+  }
+
+  getAdminInitials(): string {
+    if (this.adminInfo) {
+      const nameParts = this.adminInfo.name.split(' ');
+      const initials = nameParts.map(part => part.charAt(0).toUpperCase()).join('');
+      return initials;
+    }
+    return '';
   }
 
   @HostListener('document:click', ['$event'])
--- src/app/service/users.service.ts
+++ src/app/service/users.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IUser } from '../interface/users.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  constructor() { }
+  private baseUrl = 'http://localhost:3000/users'
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getUser(id: number): Observable<IUser>{
+    return this.http.get<IUser>(`${this.baseUrl}/${id}`);
+  }
 }
