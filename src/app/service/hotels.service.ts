f10ea96e5a5600db5eb5f8e5ce472caaebae0453
Eduardo <solanoe934@gmail.com>
2025-05-15 21:46:46 -0500

    Feat: actualizacion del servicio de hoteles

--- src/app/service/hotels.service.ts
+++ src/app/service/hotels.service.ts
@@ -18,6 +18,14 @@ export class HotelsService {
     return this.http.get<hotels[]>(this.apiUrl + '/all') 
   }
 
+  getHotelById(id: number): Observable<hotels> {
+    return this.http.get<hotels>(this.apiUrl + '/' + id)
+  }
+
+  updateHotel(hotel: hotels, id: number): Observable<hotels>{
+    return this.http.patch<hotels>(this.apiUrl + '/' + id, hotel)
+  }
+
   createHotel(hotel: hotels): Observable<hotels>{
     return this.http.post<hotels>(this.apiUrl + '/create', hotel);
   }
